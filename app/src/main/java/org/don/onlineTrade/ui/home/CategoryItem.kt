c4617d299f0f00124329007ea721eec83f85a76c
JaloliddinAbdullaev <jaloliddinabdullaev49@gmail.com>
2024-02-17 01:55:25 +0500

    reel bug is fixed

--- app/src/main/java/org/don/onlineTrade/ui/home/CategoryItem.kt
+++ app/src/main/java/org/don/onlineTrade/ui/home/CategoryItem.kt
@@ -1,6 +1,5 @@
 package org.don.onlineTrade.ui.home
 
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
@@ -8,7 +7,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -22,7 +20,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ExpandLess
 import androidx.compose.material.icons.filled.ExpandMore
-import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -34,19 +31,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import org.don.onlineTrade.BuildConfig
 import org.don.onlineTrade.data.remote.models.category.Category
 import org.don.onlineTrade.data.remote.models.category.CategoryItem
+import org.don.onlineTrade.ui.theme.robotoFontFamily
 import org.don.onlineTrade.ui.theme.spacing
 
 @Composable
@@ -94,7 +92,7 @@ fun CategoryItem(
             navigateToCategory(item.id)
         },
 
-    ) {
+        ) {
         Column(
             modifier = modifier
                 .padding(
@@ -115,9 +113,11 @@ fun CategoryItem(
             Text(
                 text = item.title,
                 textAlign = TextAlign.Center,
-                fontWeight = FontWeight.Medium,
                 fontSize = 12.sp,
-                maxLines = 1
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                fontFamily = robotoFontFamily,
+                fontWeight = FontWeight.Medium
             )
         }
     }
