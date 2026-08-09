744fa41e7d5be19517bffb2af06aab94afea17a0
JaloliddinAbdullaev <jaloliddinabdullaev49@gmail.com>
2026-08-09 20:58:06 +0200

    Bug fixes in web

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "uz.promo.selling"
         minSdk = 24
         targetSdk = 36
-        versionCode = 12
-        versionName = "1.2.1"
+        versionCode = 13
+        versionName = "1.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
